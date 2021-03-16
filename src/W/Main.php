fe5ee828e6d43ab1e83890902836da628acc32f3
HixXNetwork <70339357+HixXNetwork@users.noreply.github.com>
2021-03-16 17:21:48 +0300

    Update Main.php

--- src/W/Main.php
+++ src/W/Main.php
@@ -1,5 +1,17 @@
 <?php
+
 namespace W;
-use pocketmine\plugin\PluginBase;use pocketmine\event\Listener;use pocketmine\event\server\QueryRegenerateEvent;
-class Main extends PluginBase implements Listener{public function onEnable(){$this->getServer()->getPluginManager()->registerEvents($this, $this);}
-                public function onInrinitySlots(QueryRegenerateEvent $event){$event->setMaxPlayerCount($event->getPlayerCount() + 1);}}
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\server\QueryRegenerateEvent;
+
+class Main extends PluginBase implements Listener{
+
+                public function onEnable(){
+                $this->getServer()->getPluginManager()->registerEvents($this, $this);
+              }
+                public function onInrinitySlots(QueryRegenerateEvent $event){
+                $event->setMaxPlayerCount($event->getPlayerCount() + 1);
+              }
+}
